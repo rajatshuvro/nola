69a8bd6c6f0a7159c84410b1496775664e81221e
Rajat Roy <rroy1@illumina.com>
2021-01-04 17:13:23 -0800

    new books and checkout tests

--- src/com/nola/unitTests/subCommands/coTests.java
+++ src/com/nola/unitTests/subCommands/coTests.java
@@ -1,20 +1,12 @@
 package com.nola.unitTests.subCommands;
 
-import com.nola.databases.BookDb;
 import com.nola.databases.CheckoutDb;
-import com.nola.databases.UserDb;
-import com.nola.parsers.CheckoutCsvParser;
-import com.nola.subcommands.add;
 import com.nola.subcommands.co;
 import com.nola.unitTests.TestStreams;
-import com.nola.unitTests.testData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 import static com.nola.unitTests.testData.GetBookDb;
 import static com.nola.unitTests.testData.GetUserDb;
